babe79d3bf6ea22cbcf3d95f9cda33e6dea892d5
vikul rathod <vikul.rathod.net@gmail.com>
2023-08-12 15:23:49 +0530

    changed design for jumbotron

--- js/main.js
+++ js/main.js
@@ -44,3 +44,26 @@
     
 })(jQuery);
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+var btnModelClose = document.getElementById('btnModelClose');
+btnModelClose.onclick = function () {
+    modal.style.display = "none";
+};
+
